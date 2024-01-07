6478b53f7595c5926db310b837ebd410b55717d9
Suneet Misra <hello@suneet.codes>
2024-01-07 22:20:43 +0000

    🔧 (setup): Updates prettier configuration
    replaces existing prettier configuration with prettier-config-standard

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,7 @@
 /** @type {import("prettier").Config} */
 const config = {
-  trailingComma: 'es5',
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  printWidth: 80,
-  endOfLine: 'lf',
-  plugins: ['prettier-plugin-tailwindcss'],
+  ...require('prettier-config-standard'),
+  plugins: ['prettier-plugin-tailwindcss']
 }
 
 module.exports = config
