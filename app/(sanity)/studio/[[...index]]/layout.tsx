140e4b6db3e541c73c9e91382b3baa253f397b60
Suneet Misra <hello@suneet.codes>
2024-01-08 21:02:34 +0000

    ⚡️ Reconfigures /studio
    /studio route is now forced as static

--- app/(sanity)/studio/[[...index]]/layout.tsx
+++ app/(sanity)/studio/[[...index]]/layout.tsx
@@ -1,7 +1,21 @@
-export const metadata = {
-  title: 'One Peter Four: Studio',
-  description: 'CMS interface for OnePeterFour'
+import type { Metadata, Viewport } from 'next'
+import { metadata as studioMetadata } from 'next-sanity/studio/metadata'
+import { viewport as studioViewport } from 'next-sanity/studio/viewport'
+
+// Set the right `viewport`, `robots` and `referer` meta tags
+export const metadata: Metadata = {
+  ...studioMetadata,
+  // Overrides the title until the Studio is loaded
+  title: 'Loading Studio…'
 }
+
+export const viewport: Viewport = {
+  ...studioViewport,
+  // Overrides the viewport to resize behavior
+  interactiveWidget: 'resizes-content'
+}
+
+export const dynamic = 'force-static'
 
 export default function RootLayout({
   children
