13c3072221154ffe63a2c39508ebc0a664142ff4
Suneet Misra <hello@suneet.codes>
2024-01-28 11:29:57 +0000

    ✨ Updates footer
    adds placeholder links to footer and replaces newsletter signup with office address

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,31 +4,32 @@ import { Container } from '@/components/Container'
 import { FadeIn } from '@/components/FadeIn'
 import { Logo } from '@/components/Logo'
 import { socialMediaProfiles } from '@/components/SocialMedia'
+import { Offices } from './Offices'
 
 const navigation = [
   {
-    title: 'Work',
+    title: 'Company',
     links: [
-      { title: 'FamilyFund', href: '/work/family-fund' },
-      { title: 'Unseal', href: '/work/unseal' },
-      { title: 'Phobia', href: '/work/phobia' },
+      { title: 'Our Team', href: '/team' },
+      { title: 'Services', href: '/services' },
+      { title: 'Learning', href: '/learning' },
+      { title: 'Contact us', href: '/contact' }
+    ]
+  },
+  {
+    title: 'Partners',
+    links: [
+      { title: 'Partner 1', href: '/partners' },
+      { title: 'Partner 2', href: '/partners' },
+      { title: 'Partner 3', href: '/partners' },
       {
         title: (
           <>
             See all <span aria-hidden='true'>&rarr;</span>
           </>
         ),
-        href: '/work'
+        href: '/partners'
       }
-    ]
-  },
-  {
-    title: 'Company',
-    links: [
-      { title: 'About', href: '/about' },
-      { title: 'Process', href: '/process' },
-      { title: 'Blog', href: '/blog' },
-      { title: 'Contact us', href: '/contact' }
     ]
   },
   {
@@ -46,7 +47,7 @@ function Navigation() {
             <div className='font-display text-sm font-semibold tracking-wider text-neutral-950'>
               {section.title}
             </div>
-            <ul role='list' className='mt-4 text-sm text-neutral-700'>
+            <ul role='list' className='mt-4 text-sm text-neutral-600'>
               {section.links.map((link, linkIndex) => (
                 <li key={linkIndex} className='mt-4'>
                   <Link
@@ -65,51 +66,6 @@ function Navigation() {
   )
 }
 
-function ArrowIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox='0 0 16 6' aria-hidden='true' {...props}>
-      <path
-        fill='currentColor'
-        fillRule='evenodd'
-        clipRule='evenodd'
-        d='M16 3 10 .5v2H0v1h10v2L16 3Z'
-      />
-    </svg>
-  )
-}
-
-function NewsletterForm() {
-  return (
-    <form className='max-w-sm'>
-      <h2 className='font-display text-sm font-semibold tracking-wider text-neutral-950'>
-        Sign up for our newsletter
-      </h2>
-      <p className='mt-4 text-sm text-neutral-700'>
-        Subscribe to get the latest design news, articles, resources and
-        inspiration.
-      </p>
-      <div className='relative mt-6'>
-        <input
-          type='email'
-          placeholder='Email address'
-          autoComplete='email'
-          aria-label='Email address'
-          className='block w-full rounded-2xl border border-neutral-300 bg-transparent py-4 pl-6 pr-20 text-base/6 text-neutral-950 ring-4 ring-transparent transition placeholder:text-neutral-500 focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5'
-        />
-        <div className='absolute inset-y-1 right-1 flex justify-end'>
-          <button
-            type='submit'
-            aria-label='Submit'
-            className='flex aspect-square h-full items-center justify-center rounded-xl bg-neutral-950 text-white transition hover:bg-neutral-800'
-          >
-            <ArrowIcon className='w-4' />
-          </button>
-        </div>
-      </div>
-    </form>
-  )
-}
-
 export function Footer() {
   return (
     <Container as='footer' className='mt-24 w-full sm:mt-32 lg:mt-40'>
@@ -117,7 +73,12 @@ export function Footer() {
         <div className='grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2'>
           <Navigation />
           <div className='flex lg:justify-end'>
-            <NewsletterForm />
+            <div>
+              <h2 className='font-display text-base font-semibold'>
+                Our office
+              </h2>
+              <Offices className='mt-4 grid grid-cols-1 gap-8 sm:grid-cols-1' />
+            </div>
           </div>
         </div>
         <div className='mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12'>
