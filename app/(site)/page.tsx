040c557692ca007f2f3526bb59593a8ee03ef002
Suneet Misra <hello@suneet.codes>
2024-01-31 09:10:03 +0000

    ✨ Updates homepage
    scaffolds text-based hero

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,11 @@
 import { Metadata } from 'next'
 
-import PostsPreview from '@/components/PostsPreview'
-import Posts from '@/components/organisms/Posts'
+import { Container } from '@/components/Container'
+import { FadeIn } from '@/components/FadeIn'
 import { client } from '@/sanity/lib/client'
 import { HOMEPAGE_QUERY, POSTS_QUERY } from '@/sanity/lib/queries'
 import { loadQuery } from '@/sanity/lib/store'
 import { SanityDocument } from 'next-sanity'
-import { draftMode } from 'next/headers'
 
 const homepage = await client.fetch<SanityDocument<BasePage>>(HOMEPAGE_QUERY)
 
@@ -19,18 +18,21 @@ export default async function Home() {
   const initial = await loadQuery<SanityDocument[]>(POSTS_QUERY)
   const homePage = await client.fetch<SanityDocument<BasePage>>(HOMEPAGE_QUERY)
   return (
-    <main className='flex flex-col items-center justify-center p-24'>
-      <div className='text-center'>
-        <h1 className='py-8 font-serif text-3xl font-bold sm:text-5xl'>
-          {homePage?.heading}
-        </h1>
-        <p className='font-sans'>Coming Soon...</p>
-      </div>
-      {draftMode().isEnabled ? (
-        <PostsPreview initial={initial} />
-      ) : (
-        <Posts posts={initial.data} />
-      )}
-    </main>
+    <>
+      <Container className='mt-24 sm:mt-32 md:mt-56'>
+        <FadeIn className='max-w-3xl'>
+          <h1 className='font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod
+          </h1>
+          <p className='mt-6 text-xl text-neutral-600'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed
+            pharetra sem. Donec erat ipsum, rutrum et nisl a, vulputate aliquet
+            mauris. Praesent lorem erat, euismod ac risus ut, dapibus elementum
+            ipsum. Nunc nec sodales eros, id accumsan purus.
+          </p>
+        </FadeIn>
+      </Container>
+    </>
   )
 }
