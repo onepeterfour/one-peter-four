1bcde05e871395cf25ce0ada1f05f688117f647d
Suneet Misra <hello@suneet.codes>
2024-02-18 13:52:01 +0000

    🔧 Updates next config
    Adds output: standalone to help builds succeed on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ import { unifiedConditional } from 'unified-conditional'
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
