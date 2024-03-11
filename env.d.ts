c9a18736a424d932cfabb286f2ca2ae41f74630b
Suneet Misra <hello@suneet.codes>
2024-03-11 21:04:35 +0000

    🏷️ Adds typings for .env

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NEXT_PUBLIC_SANITY_PROJECT_ID?: string
+    NEXT_PUBLIC_SANITY_DATASET?: string
+    NEXT_PUBLIC_SANITY_API_VERSION?: string
+    SANITY_API_READ_TOKEN?: string
+  }
+}
