3673f3de92dd117d9ea3decce9e731f15885f6f0
Suneet Misra <hello@suneet.codes>
2024-03-31 08:03:07 +0100

    ♻️ (SUN-44): Renames calltoaction section component and type

--- components/PageSections/CallToAction.tsx
+++
@@ -1,6 +0,0 @@
-import type { SanityPageSectionCTA } from '@/sanity/schemas/objects/pageSections/callToActionsSection'
-
-type CallToActionProps = SanityPageSectionCTA & {}
-export const CallToAction = ({ title }: CallToActionProps) => {
-  return <div>{title}</div>
-}
