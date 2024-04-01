80d8851e7223cfee238919872be81d3325bc72ff
Suneet Misra <hello@suneet.codes>
2024-04-01 21:03:13 +0100

    ♻️ (SUN-44): Updates name of sanityPageSectionContact

--- components/PageSections/ContactSection.tsx
+++ components/PageSections/ContactSection.tsx
@@ -2,11 +2,12 @@ import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { FadeIn } from '@/components/FadeIn'
 import { Offices } from '@/components/Offices'
-import { SanityPageSectionContact } from '@/sanity/schemas/objects/pageSections/contactSection'
+import { ContactSectionSchema } from '@/sanity/schemas/objects/pageSections/contactSection'
 
-type ContactProps = SanityPageSectionContact & {}
-
-export const Contact = ({ title, buttonLabel }: ContactProps) => {
+export const ContactSection = ({
+  title,
+  buttonLabel
+}: Omit<ContactSectionSchema, 'isEnabled'>) => {
   return (
     <Container className='mt-24 sm:mt-32 lg:mt-40'>
       <FadeIn className='-mx-6 rounded-4xl bg-neutral-950 px-6 py-20 sm:mx-0 sm:py-32 md:px-12'>
