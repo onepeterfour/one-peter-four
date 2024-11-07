f6a2eb12bcb3d07b86cfcfcf0f715951bbf41abc
Suneet Misra <hello@suneet.codes>
2024-11-07 11:08:57 +0000

    💄 (SUN-151): Updates call-to-action styling

--- components/PageSections/CallToActionSection.tsx
+++ components/PageSections/CallToActionSection.tsx
@@ -5,10 +5,9 @@ import type { CallToActionSectionSchema } from '@/sanity/schemas/objects/pageSec
 
 type CallToActionProps = Omit<CallToActionSectionSchema, 'isEnabled'> & {}
 export const CallToActionSection = ({ title, link }: CallToActionProps) => {
-  console.log('CALL TO ACTION BEING USED')
   return (
-    <Container className='mt-24 sm:mt-32 lg:mt-40'>
-      <FadeIn className='max-w-fit sm:mx-0 sm:py-32 md:px-12'>
+    <Container className='mt-16'>
+      <FadeIn className='text-center'>
         <Button href={link}>{title}</Button>
       </FadeIn>
     </Container>
