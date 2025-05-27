b1c223723cfa7a46a46ba1283e566d14878f2f41
Suneet Misra <hello@suneet.codes>
2025-05-27 06:30:06 +0100

    🔧 (SUN-88): Adds environment variables for turnstile implementation

--- env.d.ts
+++ env.d.ts
@@ -4,10 +4,12 @@ declare namespace NodeJS {
     NEXT_PUBLIC_SANITY_PROJECT_ID?: string
     NEXT_PUBLIC_SANITY_DATASET?: string
     NEXT_PUBLIC_SANITY_API_VERSION?: string
+    NEXT_PUBLIC_TURNSTILE_SITE_KEY?: string
     SANITY_API_READ_TOKEN?: string
     RESEND_API_KEY?: string
     RESEND_SEND_FROM_EMAIL?: string
     RESEND_SEND_TO_EMAIL?: string
     RESEND_AUDIENCE_ID?: string
+    TURNSTILE_SECRET_KEY?: string
   }
 }
